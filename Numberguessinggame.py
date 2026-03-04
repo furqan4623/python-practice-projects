e0499ae8b54d4691faf5d3f9915562aad75b5bac
furqan ahmed <bc240423049fah@vu.du.pk>
2026-03-04 16:38:47 +0500

    Upgrade with Loops

--- Numberguessinggame.py
+++ Numberguessinggame.py
@@ -1,13 +1,27 @@
 import random
 
 secret_number = random.randint(1,100)
+attempts = 5
 
 print("Welcome to Number Guess Game!")
 print("I have selected a number between 1 to 100. ")
 
-guess = int(input("Enter your Guess. "))
+while attempts > 0:
+    guess = int(input("Enter your Guess"))
 
-if guess == secret_number:
-    print("Correct. ")
-else:
-    print("Wrong! The number was", secret_number)
+    if guess == secret_number:
+        print("Correct You Win ")
+        break
+    elif guess > secret_number:
+        print("Too High ")
+    else:
+        print("Too Low ")
+
+    attempts -= 1
+    print("Attempts Left", attempts)
+
+    if attempts == 0:
+        print("Game Over ")
+        print("The Number was is ", secret_number)
+
+        
