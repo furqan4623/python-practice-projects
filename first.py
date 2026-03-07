1c28a3b113cfe75e178b5c3e9de4605c4519a368
Furqan Ahmed <fkk4623@gmail.com>
2026-03-07 15:28:32 +0500

    changed

--- first.py
+++ first.py
@@ -6,3 +6,26 @@ second = int(input("Enter Your Second Number : "))
 Result = first + second
 
 print("Your Number is : ", Result)
+
+# Find the area of side 
+
+side = float(input("Enter Your side : "))
+
+print("Area is :", side * side)
+
+#Find the two numbers average 
+
+first = float(input("Enter Your First Number : "))
+second = float(input("Enter Your Second Number : "))
+
+average = (first + second) /2
+
+print("Your avarage is :", average)
+
+#Comparison two numbers 
+
+a = int(input("Enter First : "))
+b = int(input("Enter Second :"))
+
+Solve = a < b
+print("This is : ", Solve)
