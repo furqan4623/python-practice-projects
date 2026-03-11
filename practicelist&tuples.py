4df7a76404a42df9caccc51331eb79fabce66bf7
Furqan Ahmed <fkk4623@gmail.com>
2026-03-12 00:57:55 +0500

    Practice List

---
+++ practicelist&tuples.py
@@ -0,0 +1,21 @@
+# Praactice for list woth multiple data types
+
+movies = [] # Create List
+movies.append(input("Enter First Movie name : ")) # User Input in String
+movies.append(input("Enter second Movie name : "))
+movies.append(input("Enter third Movie name : "))
+
+print(movies) #print value
+
+# List copy method
+
+list = [1, 2, 1]
+list1 = [1, 2, 3]
+
+copy_list = list.copy()
+copy_list.reverse()
+
+if (copy_list == list):
+    print("palindrome")
+else:
+    print("not a palindrome")
