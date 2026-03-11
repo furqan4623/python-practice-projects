b250e20302679df532de0307f0e1cf6df69de449
Furqan Ahmed <fkk4623@gmail.com>
2026-03-12 00:18:12 +0500

    Learn a List

---
+++ Tuples&Lists.py
@@ -0,0 +1,30 @@
+#Creating Lists 
+marks = [85, 90, 40, 60, 80]
+print(len(marks)) # Looking for Lenght of marks 
+
+# How to Slice in list
+print(marks[0:3])
+print(marks[-4:-2]) #Negative number slicing
+
+# More builtin function in python for list
+list = [50, 80, 60, 90]
+list.append(100) # Add new Elements at the End 
+print(list)
+
+list.sort() # Sorts in Ascending order
+print(list)
+
+list.sort(reverse=True) # Sorts in Descending order
+print(list)
+
+list.reverse() # For reverse using
+print(list)
+
+list.insert(0, 40) # inser more element 
+print(list)
+
+list.remove(80) # Remove method in list for exact value not index value
+print(list)
+
+list.pop(2) # Remove the index value
+print(list)
